4c122b04ea8913020fd44296035ae8fa5f485e31
Tomi <tomi.holstila@gmail.com>
2019-06-07 09:36:06 +0200

    Polishing

--- src/TagBox.js
+++ src/TagBox.js
@@ -4,13 +4,17 @@ import ListOfTags from './ListOfTags';
 
 const constructHash = (updatedTags) => {
   // By constructing the whole hash string we will allow the potential use of other attributes in string
-  window.location.hash = `tags=${updatedTags.join(',')}`;
+  if (updatedTags.length === 0) {
+    window.location.hash = '';
+  } else {
+    window.location.hash = `tags=${updatedTags.join(',')}`
+  }
 };
 
 const TagBox = () => {
   const [tags, setTags] = React.useState([]);
   const [newTag, setNewTag] = React.useState('');
-  
+
   const getUrlHash = () => {
     const hash = window.location.hash;
     const parsedHash = qs.parse(hash.replace('#', ''));
@@ -19,36 +23,32 @@ const TagBox = () => {
 
   React.useEffect(() => {
     setTags(getUrlHash().tags ? getUrlHash().tags.split(',') : []);
-  }, []);
 
-  React.useEffect(() => {
     window.addEventListener(
       "hashchange", 
       () => {
-        console.log(getUrlHash().tags);
-        setTags(getUrlHash().tags && getUrlHash().tags.split(','))
+        setTags(getUrlHash().tags ? getUrlHash().tags.split(',') : [])
       },
       false
     );
-  }, [])
+  }, []);
+
+  const addTag = () => {
+    constructHash([
+      ...tags,
+      newTag,
+    ]);
+    setNewTag('');
+  };
 
   const removeTag = (index) => {
     constructHash([
       ...tags.slice(0, index),
       ...tags.slice(index + 1, tags.length)
     ]);
-  }
-
-  const handleClick = () => {
-    setNewTag('');
-
-    constructHash([
-      ...tags,
-      newTag,
-    ]);
   };
 
-  const handleChange = (e) => {
+  const handleInputChange = (e) => {
     setNewTag(e.target.value);
   };
 
@@ -61,11 +61,11 @@ const TagBox = () => {
           className={'TagInput'}
           type={'text'}
           value={newTag}
-          onChange={handleChange}
-          placeholder={'Enter new tag'}
+          onChange={handleInputChange}
+          placeholder={'Add new tag'}
         />
-        <button className={'TagInputButton'} type={'button'} onClick={handleClick}>
-          Enter
+        <button className={'TagInputButton'} type={'button'} onClick={addTag}>
+          Add
         </button>
       </div>
     </div>
